f68145908999e8952c9992a79a2895114a2cdf53
dando <danila.dor.1125@mail.ru>
2026-03-08 00:00:12 +0500

    DML day 1

---
+++ DML/SQLQuery1-INSERT Directions.sql
@@ -0,0 +1,19 @@
+--
+USE PV_522_DDL;
+
+--INSERT Directions
+--		(direction_id, direction_name)
+--VALUES	(1,				N'Разработка программного обеспесения');
+--INSERT Directions
+--		(direction_id, direction_name)
+--VALUES	(2,				N'Компьютерная графика и дизайн'),
+--		(3,				N'Сетевые технологии и системное администрирование');
+INSERT Directions
+VALUES	(4,				N'Java Development'),
+		(5,				N'C++ development'),
+		(6,				N'Python Development'),
+		(7,				N'GameDev'),
+		(8,				N'Android Development'),
+		(9,				N'.NET Development'),
+		(10,				N'Web Development');
+SELECT * FROM Directions;
