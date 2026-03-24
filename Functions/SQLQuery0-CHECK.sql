d87a81e24c6dc865d248358042937c209317d410
dando <danila.dor.1125@mail.ru>
2026-03-24 23:30:04 +0500

    Holidays start

--- Functions/SQLQuery0-CHECK.sql
+++ Functions/SQLQuery0-CHECK.sql
@@ -3,5 +3,8 @@ USE PV_522_Import;
 SET DATEFIRST 1;
 --PRINT dbo.GetLastLearningDate(N'PV_522');
 --EXEC	sp_SelectSchedule;
-PRINT dbo.GetNextLearningDay(N'PV_522', N'2026-03-19')
-PRINT dbo.GetNextLearningDate(N'PV_522', N'2026-03-21')
+--PRINT dbo.GetNextLearningDay(N'PV_522', N'2026-03-19')
+--PRINT dbo.GetNextLearningDate(N'PV_522', N'2026-03-21')
+
+PRINT dbo.GetNewYearHolidaysStartDate(2026);
+PRINT dbo.GetSummerHolidaysStartDate(2025);
