5f5d6e3ce9cb3dd5a26df71078c270fd7bbb27e5
dando <danila.dor.1125@mail.ru>
2026-03-24 20:19:32 +0500

    Homework 14.03

--- Functions/SQLQuery0-CHECK.sql
+++ Functions/SQLQuery0-CHECK.sql
@@ -4,3 +4,4 @@ SET DATEFIRST 1;
 --PRINT dbo.GetLastLearningDate(N'PV_522');
 --EXEC	sp_SelectSchedule;
 PRINT dbo.GetNextLearningDay(N'PV_522', N'2026-03-19')
+PRINT dbo.GetNextLearningDate(N'PV_522', N'2026-03-21')
