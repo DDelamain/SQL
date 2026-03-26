010e3fcaf31a75cc501bb599da71601e9904e22d
dando <danila.dor.1125@mail.ru>
2026-03-26 23:49:32 +0500

    Holidays DONE

--- Functions/SQLQuery0-CHECK.sql
+++ Functions/SQLQuery0-CHECK.sql
@@ -6,5 +6,12 @@ SET DATEFIRST 1;
 --PRINT dbo.GetNextLearningDay(N'PV_522', N'2026-03-19')
 --PRINT dbo.GetNextLearningDate(N'PV_522', N'2026-03-21')
 
-PRINT dbo.GetNewYearHolidaysStartDate(2026);
-PRINT dbo.GetSummerHolidaysStartDate(2025);
+--PRINT dbo.GetNewYearHolidaysStartDate(2026);
+--PRINT dbo.GetSummerHolidaysStartDate(2025);
+--PRINT dbo.GetEasterDate(2026);
+PRINT dbo.GetHolidaysStartDate(2026, N'Новогодние%');
+PRINT dbo.GetHolidaysStartDate(2026, N'23%');
+PRINT dbo.GetHolidaysStartDate(2026, N'8%');
+PRINT dbo.GetHolidaysStartDate(2026, N'Пасха');
+PRINT dbo.GetHolidaysStartDate(2026, N'Майские%');
+PRINT dbo.GetHolidaysStartDate(2026, N'Летние%');
